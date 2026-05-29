e53da0f2c62f032b35d08417f3e9177cd8263998
alisaliim85 <alisaliim85@gmail.com>
2026-05-29 23:00:20 +0300

    feat(notifications): scaffold app skeleton

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'brokers',
     'service_requests',
     'medications',
+    'notifications',
 ]
 
 MIDDLEWARE = [
---
+++ notifications/urls.py
@@ -0,0 +1,7 @@
+# notifications/urls.py
+from django.urls import path
+from . import views
+
+app_name = 'notifications'
+
+urlpatterns = []
